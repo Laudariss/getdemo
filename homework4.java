22f94e4aa113a3871663eba2b38c6d7248660df0
Laudariss <82233782+Laudariss@users.noreply.github.com>
2021-08-15 17:03:19 +0800

    cat2

--- homework4.java
+++ homework4.java
@@ -4,7 +4,7 @@ public class homework4 {
 public static void main(String args[]) {
  Scanner sc = new Scanner(System.in);
  int[][] array = {{25,32,8,19,27},{2500,800,500,1000,1200}};
-   System.out.println("小華欲借的金額");
+   System.out.println("小華欲借的money");
    int a = sc.nextInt();//輸入欲借的金額
     int count =0;
     System.out.println("可借錢的員工編號:");
